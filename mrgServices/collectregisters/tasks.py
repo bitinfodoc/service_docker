8cf01a4707c30bfd7b059ce84eefbeb64679bb06
bitinfodoc <bitinfodoc@yandex.ru>
2023-05-31 11:38:19 +0500

    Receipts from site autoupload correct email

--- mrgServices/collectregisters/tasks.py
+++ mrgServices/collectregisters/tasks.py
@@ -38,23 +38,5 @@ def collectReceipts():
         message_body = 'Заявки на квитанции с сайта во вложении',
         message_file = file,
         message_sender = 'zayavki@orenburgregiongaz.ru',
-        message_recipient = ['p.blagovisnyi@mail.org056.ru']
-        # message_recipient = ['s.stepanov@mail.org056.ru','p.blagovisnyi@mail.org056.ru']
+        message_recipient = ['s.stepanov@mail.org056.ru','p.blagovisnyi@mail.org056.ru']
     )
-
-# @shared_task
-# def collectReadingsShared():
-
-#     file  = getReadingsFromSite()
-#     sendEmailMessage(
-#         message_title = 'Показания с сайта',
-#         message_body = 'Показания с сайта во вложении',
-#         message_file = file,
-#         message_sender = 'pok@orenburgregiongaz.ru',
-#         # message_recipient = ['oplata@mail.org056.ru','p.blagovisnyi@mail.org056.ru']
-#         message_recipient = ['p.blagovisnyi@mail.org056.ru']
-#     )
-
-#     task = PeriodicTask.objects.get(name='Repeat order {}')
-#     task.enabled = True
-#     task.save()
