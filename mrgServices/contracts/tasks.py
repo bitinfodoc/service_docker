7ba13f028e57bbaccfc515a03effa5bb25cdf27d
bitinfodoc <bitinfodoc@yandex.ru>
2023-10-27 06:07:06 +0500

    Fix upload VDGO

--- mrgServices/contracts/tasks.py
+++ mrgServices/contracts/tasks.py
@@ -14,19 +14,21 @@ def createContractVdgoRecord(current_file_name):
             break
         try:
             records = ContractVdgo.objects.filter(account_number = line_result[0])
-            # print(records.values())
+            print(line_result[0])
+            print(line_result[1])
+            print(line_result[2])
 
             if len(records) == 0:
                 ContractVdgo(
                     account_number = str(line_result[0]).strip(),
                     account_number_rng = str(line_result[1]).strip(),
-                    account_address = str(line_result[2]).strip()
+                    account_address = str(line_result[2])
                 ).save()
             else:
                 print('already exist')
         except:
             file.close
-            print("Can't write LsRecord")
+            print("Can't write Contract VDGO Record")
             pass
 
     file.close
